c699f01aca705f9a28506c3d392d487b16e9e992
komunre <koliziy5@gmail.com>
2020-06-08 11:30:29 +0700

    Comments for .h files

--- src/headers/bot.h
+++ src/headers/bot.h
@@ -15,26 +15,26 @@ public:
 	int GetRepl() { return Replications; }
 	void GiveEnergy(int amount);
 	std::string GetGen();
-	bool Exists;
+	bool Exists; //Anyway Bot exists in array but if bo doesn't exists he does'nt drawing and commands not exected
 	bool Alive = true;
 	void Mutate();
-	int PhsCount = 1;
-	int EatCount = 0;
+	int PhsCount = 1; //For color
+	int EatCount = 0; //For color
 	bool Infected = false;
 	bool Immunity = false;
 	void ChangeGen(int cmdNum, int value);
 private:
 	int age = 0;
-	int cmds[64];
-	int Energy = 1000;
-	int turn = 0;
-	int Replications = 0;
+	int cmds[64]; //commands for execution
+	int Energy = 1000; //Energy for ations
+	int turn = 0; //Current command num
+	int Replications = 0; //Replications counter for mutations
 	void Phs(int y);
 	void Replicate(int x, int y, Bot **bots, int width, int height);
 	void Eat(int x, int y, Bot **bots, int width, int height);
 	void Move(int x, int y, Bot **bots, int width, int height);
 	void Share(int x, int y, Bot **bots, int width, int height);
 	void Cough(int x, int y, Bot **bots);
-	int DefaultEnergy = 1000;
+	int DefaultEnergy = 1000; //Enegy gived after birth
 	int InfectChance = 30;
 };
--- src/headers/field.h
+++ src/headers/field.h
@@ -9,11 +9,11 @@ public:
 	void ExecuteAllCmds();
 	void AddBot(int x, int y, Bot SomeBot);
 	void Draw(sf::RenderWindow &window);
-	const int height = 70;
-	const int width = 100;
+	const int height = 70; //Field height
+	const int width = 100; //Field width
 private:
-	Bot **bots;
+	Bot **bots; //Field of the bots
 	int InfectionCounter;
-	int InfectionTime = 300;
-	int sleepMs = 0;
+	int InfectionTime = 300; //Every n turns infect one bot
+	int sleepMs = 0; //Slow down process
 };
